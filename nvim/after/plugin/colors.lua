d2f7d38c91a4e307adbcc8fb4d09a8a05ee50a1f
Oliver Sørensen <112586661+Katikistan@users.noreply.github.com>
2024-12-18 23:13:25 +0100

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,5 +1,5 @@
 -- colors.lua
-vim.g.catppuccin_flavour = "frappe" -- You can choose between 'latte', 'frappe', 'macchiato', 'mocha'
+vim.g.catppuccin_flavour = "mocha" -- You can choose between 'latte', 'frappe', 'macchiato', 'mocha'
 
 -- Set the colorscheme
 vim.cmd [[colorscheme catppuccin]]
