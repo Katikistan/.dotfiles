62be99ff22161790436b0e6724e19b0975b029f7
Katikistan Skjern <katikistan@debian.katikistan>
2025-06-25 15:58:51 +0200

    i3 status etc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo usermod -aG sudo katikistan
-
+cd ..
 # Update package list
 sudo apt update && sudo apt upgrade -y
 
@@ -19,17 +19,26 @@ sudo apt install -y zsh
 # Install Alacritty terminal (with dependencies)
 sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
 sudo apt install -y alacritty
+<<<<<<< HEAD
+# Install Rust programming language
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+=======
 
 # Install Rust 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+>>>>>>> refs/remotes/origin/main
 
 # Install net-tools (for networking tools like ifconfig)
 sudo apt install -y net-tools
 
 # Install Nmap for network exploration
 sudo apt install -y nmap
+<<<<<<< HEAD
+sudo apt install -y blueman
+=======
 
 sudo apt install -y blueman 
+>>>>>>> refs/remotes/origin/main
 # Install netcat (utility for reading and writing network connections)
 sudo apt install -y netcat-openbsd
 
@@ -39,11 +48,11 @@ sudo apt install -y wireshark
 # Install htop (interactive process viewer)
 sudo apt install -y htop
 
-# Install neofetch (system information tool)
-sudo apt install -y neofetch
-
 # Install Git (version control system)
 sudo apt install -y git
+
+# tmux 
+sudo apt install tmux
 
 # Install i3 window manager with necessary utilities
 sudo apt install -y i3status dmenu xbacklight feh
@@ -59,16 +68,28 @@ tar -xzf jdk-21*.tar.gz
 sudo mv jdk-21* /opt/
 source ~/.zshrc
 
+<<<<<<< HEAD
+# Install Firefox web browser (optional)
+# sudo apt install -y firefox
+=======
 # python
 sudo apt-get install python3-pip
 
 
 sudo apt install -y vlc
 sudo apt install -y firefox
+>>>>>>> refs/remotes/origin/main
 
 # nvim
 sudo apt-get install -y ninja-build gettext cmake unzip curl build-essential
 git clone https://github.com/neovim/neovim
+<<<<<<< HEAD
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo 
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux-x86_64.deb
+cd ~
+# Install Docker
+sudo apt install -y docker.io
+=======
 cd neovim 
 git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo 
@@ -76,6 +97,7 @@ cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 cd ~
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+>>>>>>> refs/remotes/origin/main
 
 # install docker
 sudo apt install -y docker.io
--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,5 +1,9 @@
 -- colors.lua
+<<<<<<< HEAD
+vim.g.catppuccin_flavour = "mocha"
+=======
 vim.g.catppuccin_flavour = "mocha" -- You can choose between 'latte', 'frappe', 'macchiato', 'mocha'
+>>>>>>> refs/remotes/origin/main
 
 -- Set the colorscheme
 vim.cmd [[colorscheme catppuccin]]
