206e6d5c275ad7d00d317b85544b37441b8a3a69
katikistan <katikistan@tuta.io>
2026-01-02 17:25:39 +0100

    added new configs

---
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -0,0 +1,10 @@
+-- colors.lua
+<<<<<<< HEAD
+vim.g.catppuccin_flavour = "mocha"
+=======
+vim.g.catppuccin_flavour = "mocha" -- You can choose between 'latte', 'frappe', 'macchiato', 'mocha'
+>>>>>>> refs/remotes/origin/main
+
+-- Set the colorscheme
+vim.cmd [[colorscheme catppuccin]]
+
